b035f4ae3db1530fd187854866ee5f617368b03c
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-12-14 15:00:22 +0100

    logitem and preloader components added

---
+++ src/components/logs/LogItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Moment from 'react-moment';
+
+const LogItem = ({ log }) => {
+  return (
+    <li className="collection-item">
+      <a
+        href="#edit-log-modal"
+        className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}
+      >
+        {log.message}
+      </a>
+      <br />
+      <span className="grey-text">
+        <span className="black-text">ID #{log.id}</span>
+        last updated by <span className="black-text">{log.text}</span> on{' '}
+        <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
+      </span>
+      <a href="#!" className="secondary-content">
+        <i className="material-icons grey-text">Delete</i>
+      </a>
+    </li>
+  );
+};
+
+export default LogItem;
