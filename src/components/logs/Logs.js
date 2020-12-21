2e974d29cf50da7edc3fda37a80561e7f2a1eaad
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-12-21 18:44:47 +0100

    connecting redux to component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     // eslint-disable-next-line
@@ -20,7 +19,7 @@ const Logs = () => {
     setLoading(false);
   };
 
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
@@ -38,4 +37,8 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+const mapStateToProps = state => ({
+  log: state.log,
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
