316f92fad02c430686aa892283717cf11c5e72ff
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-12-27 23:27:32 +0300

    searchlog redux implemented

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import { searchLogs } from '../../actions/logActions';
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('');
+
+  const onChange = e => {
+    searchLogs(text.current.value);
+  };
+
   return (
     <nav style={{ marginBottom: '30px' }} className="blue">
       <div class="nav-wrapper">
         <form>
           <div class="input-field">
-            <input id="search" type="search" />
+            <input
+              id="search"
+              type="search"
+              placeholder="Search Logs..."
+              ref={text}
+              onChange={onChange}
+            />
             <label class="label-icon" for="search">
               <i class="material-icons">search</i>
             </label>
@@ -18,4 +32,4 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+export default connect(null, { searchLogs })(SearchBar);
